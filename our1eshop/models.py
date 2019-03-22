b249c3177f09c133a65c5896c73123b66e32be0a
YStrength <lcllysz@outlook.com>
2019-03-22 12:43:02 +0800

    2019.3.22 update

--- our1eshop/models.py
+++ our1eshop/models.py
@@ -16,3 +16,16 @@ class EshopCommodityInfo(models.Model):
         db_table = 'e_shop_commodity_info'
         ordering = ['id']
 
+class EshopUserInfo(models.Model):
+    userId = models.IntegerField(unique=True)       #用户ID
+    #头像dir
+    username = models.CharField(max_length=15)      #用户名
+    password = models.CharField(max_length=16)      #密码
+    tel = models.IntegerField(max_length=11)        #手机号
+    email = models.CharField(max_length=32)                      #邮箱
+    locainfo = models.TextField(max_length=50)                   #地域信息
+    reg_time = models.TimeField(max_length=8)                   #注册日期
+    #权限
+    #登录状态
+    #账号状态
+
